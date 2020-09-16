499eb499794a4793a6b85bdff851112b1cedadec
Kizito25 <Kizito25@gmail.com>
2020-09-16 01:21:59 +0100

    create new message template

--- utils/msgTemplate.js
+++ utils/msgTemplate.js
@@ -1,27 +1,4 @@
-// Console.log Shorthand
-const cl = require("../utils/consoleLog");
-const nodemailer = require("nodemailer");
-
-// Message Sender
-exports.sendMessage = (req, res, next) => {
-  const hostName = req.hostname;
-  const currentURL = req.originalUrl;
-  const port = process.env.PORT || 3000;
-  const fullURL = hostName + ":" + port + currentURL;
-  cl.log(fullURL);
-  // const newMessage = `
-  //   <p> You have  a new contact request </p>
-  //   <h3> Contact details </h3>
-  //   <ul>
-  //       <li> Name: ${req.body.name}</li>
-  //       <li> Email: ${req.body.email} </li>
-  //       <li> Message: ${req.body.message} </li>
-  //   </ul>
-  //   <h3> Message: </h3>
-  //   <p><strong> ${req.body.message} </strong></p>
-  //   `;
-
-  const newMessage = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+const message = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:o="urn:schemas-microsoft-com:office:office" xmlns:v="urn:schemas-microsoft-com:vml">
 <head>
@@ -553,71 +530,5 @@ exports.sendMessage = (req, res, next) => {
 <!--[if (IE)]></div><![endif]-->
 </body>
 </html>`;
-  // Google Transport
-  //   const transport = nodemailer.createTransport({
-  //     service: "gmail",
-  //     auth: {
-  //       user: "dev@gmail.com",
-  //       pass: process.env.EMAIL_PASS,
-  //     },
-  //     tls: {
-  //       rejectUnauthorized: false,
-  //     },
-  //   });
 
-  // Etheareal Transport
-  //   const transport = nodemailer.createTransport({
-  //     host: "smtp.ethereal.email",
-  //     port: 587,
-  //     auth: {
-  //       user: "myra11@ethereal.email",
-  //       pass: "H4fEHeaxYA7RteYR9Z",
-  //     },
-  //   });
-
-  // Personal Host
-  const transport = nodemailer.createTransport({
-    host: process.env.NODE_MAILER_HOST,
-    port: 587,
-    auth: {
-      user: process.env.NODE_MAILER_EMAIL,
-      pass: process.env.NODE_MAILER_PASSWORD,
-    },
-    allowUnauthorized: true,
-  });
-
-  let mailOptions = {
-    //sender address
-    from: ' "My Portfolio" <admin@maxfixdelivery.com>',
-
-    //list of recievers
-    to: "devkizito@gmail.com",
-
-    //subject line
-    subject: `New Message from: ${req.body.fullname}`,
-
-    //Plain Text Body
-
-    //text: 'Hello',
-
-    //html Body
-    html: newMessage,
-  };
-
-  transport.sendMail(mailOptions, (error, info) => {
-    if (error) {
-      console.log(error);
-      return res.redirect("back");
-    }
-    console.log("Message sent", info.messageId);
-
-    console.log("Preview Url: %s", nodemailer.getTestMessageUrl(info));
-    res.redirect(fullURL);
-  });
-};
-
-// Message Receiver
-exports.receiveMessage = (req, res, next) => {
-  cl.log("Message sent!");
-  cl.log(typeof req.url);
-};
+module.exports = message;
