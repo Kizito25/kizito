6dfbc5829277c6148d52b21f76fbb11010b1a389
Kizito25 <Kizito25@gmail.com>
2020-12-13 21:55:19 +0100

    Fixed DataBase

--- index.js
+++ index.js
@@ -12,15 +12,15 @@ const cl = require("./utils/consoleLog");
 
 // Connect to Database
 mongoose
-  .connect(process.env.MONGO_URI_LOCAL, {
+  .connect(process.env.MONGO_URI_REMOTE, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
   })
   .then((connected) => {
     cl.log("Connected to Database");
-    // const PORT = process.env.PORT || 3000;
-    // app.listen(PORT, () => cl.log(`server listening on port ${PORT}`));
+    const PORT = process.env.PORT || 3000;
+    app.listen(PORT, () => cl.log(`server listening on port ${PORT}`));
   })
   .catch((err) => {
     cl.log(err);
@@ -65,7 +65,7 @@ app.use("/messenger", msgRoute);
 app.use((req, res, next) => {
   res.status(404).send("error 404! Page cannot be found");
 });
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => cl.log(`server listening on port ${PORT}`));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => cl.log(`server listening on port ${PORT}`));
 
 module.exports = app;
